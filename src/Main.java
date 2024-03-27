5c75a53382f546385f7485c0c0789c9a4f01a100
MarkusJCollection <markusjosephj@gmail.com>
2024-03-27 15:14:57 -0500

    Added distance class.

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,67 @@
+import java.util.Scanner;
 public class Main {
+/*
+    There are 3 types of conversion the company needs to plan:
+    1. Distances - km,mi,etc
+    2. Weights - kg,lb,etc
+    3. Currency - USD,EUR,etc
+
+    We need an app which asks what conversion needs
+    to be made from the above 3 choices.
+
+    Asks for the first value & measurement to be converted
+    (i.e. 12 lbs. or 1200 YEN) then
+    provides a list of choices based on that to convert to
+    (if just one option – this step can be skipped)
+    and performs and outputs the conversion value.
+
+    It would then ask if the user wants a new conversion and
+    restart at the beginning if they state yes (close if not).
+ */
+
+//Global scanner function.
+    static Scanner scan = new Scanner(System.in);
+
+
+//Distance Function
+
+
+
+
+
+
+//Weight Function
+
+
+
+
+
+
+//Currency Function
+
+
+
+
+
+
+//Main program
     public static void main(String[] args) {
+
+
         System.out.println("Hello world!");
         System.out.println("test");
         System.out.println("test2");
+
+
     }
+
+
+
+
+
+
+
+
+
+
 }
