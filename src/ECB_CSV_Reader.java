5b1d57f0e5a7847145281fccb44bb65048314d96
MarkusJCollection <markusjosephj@gmail.com>
2024-04-30 12:18:47 -0500

    Started user input class.

---
+++ src/ECB_CSV_Reader.java
@@ -0,0 +1,39 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.List;
+
+public class ECB_CSV_Reader {
+    Path CSVFile = Paths.get("data/eurofxref.csv");
+    HashMap<String,Double> conversionsMap = new HashMap<>();
+    String date;
+
+    private void fileLoader (){
+        List<String> file = null;
+        try{
+            file = Files.readAllLines(this.CSVFile);
+        }catch(IOException ex){
+            System.out.println("Could not read file.");
+        }
+
+        String[] keys = file.get(0).split(",");
+        String[] vals = file.get(1).split(",");
+
+        for(int i=1;i<keys.length-1;i++){
+            this.conversionsMap.put(keys[i],Double.parseDouble(vals[i]));
+        }
+        this.date = vals[0];
+
+        System.out.println(this.conversionsMap);
+        System.out.println(this.date);
+    }
+
+
+    public ECB_CSV_Reader(){
+        fileLoader();
+    }
+
+
+}
