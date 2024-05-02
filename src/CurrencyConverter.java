e0770aa9a1e218c032ae646ebe5323b6d3209ccf
MarkusJCollection <markusjosephj@gmail.com>
2024-05-01 21:31:46 -0500

    Some formatting changes.

--- src/CurrencyConverter.java
+++ src/CurrencyConverter.java
@@ -15,25 +15,26 @@ public class CurrencyConverter {
         //All units centered around the Euro.
 
             //Reads the ECB CSV file that contains
-            // up-to-date exchange rates.
+            // up-to-date exchange rates. Euro added
+            // manually.
         ECBReader conversions = new ECBReader();
         this.conversionMap = conversions.conversionsMap;
         this.conversionMap.put("EUR",1.0);
-            //Looked up how forEach worked and suggested
-            // the use of lambda.
 
+            //Foreach loop adding each key entry to a list.
         for(String key : this.conversionMap.keySet()){
             this.availableConversions.add(key);
         }
 
-        //?????
+        //????? Look into later to understand what this is
         //this.conversionMap.forEach((key, value) -> {
         //    this.availableConversions.add(key);
         //});
     }
 
+
     /**
-     * Prints out a formatted list of available conversions.
+     * Prints a list of available conversions in a table.
      */
     public void availableConversions(){
             //Definition of variables used.
@@ -61,23 +62,36 @@ public class CurrencyConverter {
     }
 
 
+    /**
+     * Function that will convert a given dollar amount
+     * from one currency to another.
+     * @param amt Dollar amount input
+     * @param code1 Currency you're converting from.
+     * @param code2 Currency you're converting to.
+     */
     public void convert(Double amt, String code1,
                         String code2) {
-        Double newNum;
+            //Definition of variables used.
         Double firstMult;
         Double secMult;
         amt = amt*100;
 
-        try{
-            firstMult = this.conversionMap.get(code1)*100;
-            secMult = this.conversionMap.get(code2)*100;
-            amt = ((amt/firstMult)*secMult)/100;
-            System.out.printf("%.2f",amt);
-        }catch(NullPointerException ex){
-            System.out.println("Invalid currency code.");
-        }catch(Exception ex){
-            System.out.println("uh oh");
-            System.out.println(ex);
+            //Try-catch that will tell you if an input
+            // is invalid, nested in a while loop to
+            // allow the user to retry.
+        while(true) {
+            try {
+                firstMult = this.conversionMap.get(code1) * 100;
+                secMult = this.conversionMap.get(code2) * 100;
+                amt = ((amt / firstMult) * secMult) / 100;
+                System.out.printf("%.2f", amt);
+                break;
+            } catch (NullPointerException ex) {
+                System.out.println("Invalid currency code.");
+            } catch (Exception ex) {
+                System.out.println("uh oh, add this to the exception list");
+                System.out.println(ex);
+            }
         }
     }
 
--- src/UserInput.java
+++ src/UserInput.java
@@ -22,19 +22,40 @@ public class UserInput {
         return sc.nextLine();
     }
 
+
+
+    public int askWhich(){
+        System.out.printf("""
+                What converter would you like to use? (1,2,3)
+                1: Currency Converter
+                2: Weights Converter
+                3: Distance Converter\n""");
+        try{
+            return Integer.parseInt(sc.nextLine());
+        }catch(NumberFormatException ex){
+            System.out.println("Invalid choice.");
+            return 0;
+        }catch(Exception ex){
+            System.out.println(ex);
+            return 0;
+        }
+    }
+
+
+
     public void askUser(){
-        userChoice[0] = askNum();
-        userChoice[1] = askFROMConversion();
-        userChoice[2] = askTOConversion();
+        this.userChoice[0] = askNum();
+        this.userChoice[1] = askFROMConversion();
+        this.userChoice[2] = askTOConversion();
     }
     public UserInput(){
     }
 
 
     public UserInput(String num, String c1, String c2){
-        userChoice[0] = num;
-        userChoice[1] = c1;
-        userChoice[2] = c2;
+        this.userChoice[0] = num;
+        this.userChoice[1] = c1;
+        this.userChoice[2] = c2;
     }
 
 
