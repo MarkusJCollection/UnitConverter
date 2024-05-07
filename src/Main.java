9488fd972aaec2f454f5ffcea3510cf23757f045
MarkusJCollection <markusjosephj@gmail.com>
2024-05-07 12:10:25 -0500

    Some work done with main and other files.

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,11 @@
+import java.util.HashMap;
 import java.util.Scanner;
 public class Main {
 /*
     There are 3 types of conversion the company needs to plan:
-    1. Distances - km,mi,etc
-    2. Weights - kg,lb,etc
-    3. Currency - USD,EUR,etc
+    1. Distances - km,mi,etc.        CENTERED AROUND METER
+    2. Weights - kg,lb,etc.          CENTERED AROUND ????
+    3. Currency - USD,EUR,etc.       CENTERED AROUND EURO
 
     We need an app which asks what conversion needs
     to be made from the above 3 choices.
@@ -19,29 +20,70 @@ public class Main {
     restart at the beginning if they state yes (close if not).
  */
 
-//Global scanner function.
+//Global functions.
     static Scanner scan = new Scanner(System.in);
+    static CurrencyConverter currencyConverter = new CurrencyConverter();
+    static DistanceConverter distanceConverter = new DistanceConverter();
+    static UserInput input = new UserInput();
+
+
+    /*
+
+     */
+    public static String ask(){
+        String output;
+        switch(input.askWhich()){
+            case 1:
+                while(true){
+                    currencyConverter.availableConversions();
+                    input.askUser();
+                    try{
+                        output = currencyConverter.convert(Double.parseDouble(input.userChoice[0])
+                                ,input.userChoice[1],input.userChoice[2]);
+                    }catch(NumberFormatException ex){
+                        output = "Error, starting value must be a number.";
+                    }catch(Exception ex){
+                        output = "Error, "+ex;
+                    }
+
+                    if(output.contains("Error")){
+                        System.out.println(output+"\n");
+                    }else{
+                        return output;
+                    }
+                }
+          case 2:
+              while(true){
+                  distanceConverter.availableConversions();
+                  input.askUser();
+                  try{
+                      output = distanceConverter.convert(Double.parseDouble(input.userChoice[0])
+                              ,input.userChoice[1],input.userChoice[2]);
+                  }catch(NumberFormatException ex){
+                      output = "Error, starting value must be a number.";
+                  }catch(Exception ex){
+                      output = "Error, "+ex;
+                  }
+
+                  if(output.contains("Error")){
+                      System.out.println(output+"\n");
+                  }else{
+                      return output;
+                  }
+              }
+            case 3:
+                return "Not implemented.";
+            default:
+                System.out.println("Error.");
+        }
+        return null;
+    }
 
 
 
 
 //Main program
     public static void main(String[] args) {
-        CurrencyConverter currencyConverter = new CurrencyConverter();
-
-        double output = currencyConverter.Convert(2,"USD","EUR");
-        System.out.println(output);
-
-
+        System.out.println(ask());
     }
-
-
-
-
-
-
-
-
-
-
 }
