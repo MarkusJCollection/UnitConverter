279ef83582909d5169e26cfaf89c0bb6e55c89a6
MarkusJCollection <markusjosephj@gmail.com>
2024-10-26 19:19:43 -0500

    Add comments to ECB API user.

--- src/GenFileReader.java
+++ src/GenFileReader.java
@@ -27,26 +27,37 @@ public class GenFileReader {
      * File loader for the currency converter.
      */
     private void fileLoaderCUR(){
+        //Bad formatting, but as a basic idea it is fine for now.
         try{
+                //Initialize the URI variable.
             URI url = new URI("https://data-api.ecb.europa.eu/service/data/EXR/D..EUR.SP00.A?lastNObservations=1&detail=dataonly&format=csvdata");
-            HttpURLConnection conn = (HttpURLConnection) url.toURL().openConnection();
-            conn.setRequestMethod("GET");
-            conn.connect();
+            HttpURLConnection conn = (HttpURLConnection) url.toURL().openConnection(); //Intialize connection variable.
+            conn.setRequestMethod("GET"); //Connect as a GET for information.
+            conn.connect(); //Connect to the API.
 
-            int responseCode = conn.getResponseCode();
+            int responseCode = conn.getResponseCode(); //Response code received, ECB has all available.
 
             if (responseCode != 200){
+                    //If it fails to connect then it gives an exception.
                 throw new RuntimeException("HTTPResponseCode: "+responseCode);
             } else {
+                    //If the connection was successful, then a scanner starts
+                    // grabbing all information received.
                 Scanner scanner = new Scanner(url.toURL().openStream());
+                    //Scans a line to skip.
                 scanner.nextLine();
+                    //While loop that goes through every line.
                 while (scanner.hasNextLine()){
+                        //Sets the line equal to an array.
                     String[] fileLine = scanner.nextLine().split(",");
+                        //Adds the currency identifier (2) and rate (7) into the
+                        // conversion hashmap.
                     conversionsMapCUR.put(fileLine[2],Double.parseDouble(fileLine[7]));
+                        //Sets the date.
                     this.dateCUR = fileLine[6];
                 }
+                    //Closes the scanner after finishing.
                 scanner.close();
-                System.out.print(conversionsMapCUR);
             }
         }catch(Exception e){
             e.printStackTrace();
